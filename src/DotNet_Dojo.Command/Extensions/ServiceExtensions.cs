e16533a95b1801777ade0b742c7433a926ab2e4c
Tom <Tom@CosmicSafari.co.uk>
2021-10-09 23:43:12 +0100

    Built service extension for mediatr references

---
+++ src/DotNet_Dojo.Command/Extensions/ServiceExtensions.cs
@@ -0,0 +1,17 @@
+﻿using DotNet_Dojo.Command.UseCases.Reference.Handlers;
+using MediatR;
+using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
+
+namespace DotNet_Dojo.Domain.Extensions
+{
+    public static class ServiceExtensions
+    {
+        public static IServiceCollection AddMediatorReferences(this IServiceCollection services)
+        {
+            var mediatrAssembly = Assembly.GetAssembly(typeof(GetExampleReference));
+            
+            return services.AddMediatR(mediatrAssembly);
+        }
+    }
+}
