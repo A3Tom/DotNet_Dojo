4d983d96c94088d048550312386805c8b5f88384
Tom <Tom@CosmicSafari.co.uk>
2021-10-09 23:43:58 +0100

    Added reference service

---
+++ src/DotNet_Dojo.Application/Services/ReferenceService.cs
@@ -0,0 +1,36 @@
+﻿using DotNet_Dojo.Application.Services.Abstract;
+using DotNet_Dojo.Domain.Dtos;
+using System.Threading.Tasks;
+
+namespace DotNet_Dojo.Application.Services
+{
+    public class ReferenceService : IReferenceService
+    {
+        private readonly object _referenceLock = new();
+        private ReferenceDto _reference;
+
+        public async Task<bool> SetReference(ReferenceDto reference)
+        {
+            return await Task.Run(() =>
+            {
+                lock (_referenceLock)
+                {
+                    _reference = reference;
+                    
+                    return _reference == reference;
+                }
+            });
+        }
+
+        public async Task<ReferenceDto> GetReference()
+        {
+            return await Task.Run(() =>
+            {
+                lock (_referenceLock)
+                {
+                    return _reference;
+                }
+            });
+        }
+    }
+}
